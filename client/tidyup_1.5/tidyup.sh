6148338caa455efd0ef31f22da5fd056d0f29b21
Daniel <technikamateur@gmx.de>
2017-04-11 15:44:54 +0200

    Revert "some small fixes"
    This reverts commit d3026fad9b8fe039c4753fdc70d77c8ad8923cd6.

--- client/tidyup_1.5/tidyup.sh
+++ client/tidyup_1.5/tidyup.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
-#
-# tidyup - a linux shell program to clean your Banana Pi and Raspberry Pi.
-# Copyright (C) 2016  Daniel Körsten (contact: technikamateur@gmx.de)
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# version
+# This work is licensed under the Creative Commons Attribution 4.0 International License.
+# To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/
+# or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+# setting up some basic parameters
 version="1.5"
 wai=$(dirname "$(readlink -e "$0")")
 internet=true
@@ -24,66 +10,70 @@ internet=true
 clear
 echo "Welcome to tidyup v$version by TechnikAmateur
 Press Enter to continue"; read
-if [[ $EUID != 0 ]]; then
-  echo "Please run as superuser"
-  exit 1
+if (( $EUID != 0 ))
+then
+    echo "Please run as superuser"
+    exit 1
 fi
 echo
 # checking directory
-if ! [[ "$wai" == "/usr/local/bin" ]]; then
-  echo "Copying to /usr/local/bin"
-  echo 'Start script using "tidyup" wherever you are'
-  cp tidyup.sh /usr/local/bin
-  rm tidyup.sh
-  cd /usr/local/bin
-  cp tidyup.sh tidyup
-  chmod 755 tidyup
-  rm tidyup.sh
-  exit 0
+if ! [ "$wai" == "/usr/local/bin" ]
+then
+    echo "Copying to /usr/local/bin"
+    echo 'Start script using "tidyup" wherever you are'
+    cp tidyup.sh /usr/local/bin
+    rm tidyup.sh
+    cd /usr/local/bin
+    cp tidyup.sh tidyup
+    chmod a+x tidyup
+    rm tidyup.sh
+    exit 1
 fi
 # checking for curl
-if ! [[ -f /usr/bin/curl ]]; then
-  echo -e "We need to install curl first... (please wait)"
-  apt-get update > /dev/null && apt-get install -y curl
-  echo
+if ! [ -f /usr/bin/curl ]
+then
+        echo -e "We need to install curl first... (please wait)"
+        apt-get update && apt-get install -y curl
+        echo
 fi
 # checking for update
 echo "Checking for Updates..."
-curl -s --request GET https://raw.githubusercontent.com/technikamateur/tidyup/master/update.info || $internet=false
-if [[ $internet == true ]]; then
-  wget -N -q https://raw.githubusercontent.com/technikamateur/tidyup/master/update.info
-  lversion="$(cat update.info)"
-  if not [[ $lversion == $version ]]; then
-    clear
-    echo "Updating..."
-    wget -N -q https://github.com/technikamateur/tidyup/releases/download/$lversion/tidyup.sh
-    cp --remove-destination tidyup.sh tidyup
-    chmod 755 tidyup
-    rm tidyup.sh update.info > /dev/null
-    echo "Please restart script!"
-    exit 0
+curl -s --request GET http://technikamateur.bplaced.net/tidyup/check.php?userversion=$version || $internet=false
+if [ $internet == true ]
+then
+  update=$(curl -s --request GET http://technikamateur.bplaced.net/tidyup/check.php?userversion=$version)
+  if [ "$update" == "latest-version" ]
+  then
+      echo "You have got the latest-version!"
   else
-    rm update.info > /dev/null
+      wget -N -q $update/tidyup.sh
+      echo "Updating..."
+      cp --remove-destination tidyup.sh tidyup
+      chmod a+x tidyup
+      rm tidyup.sh
+      echo "Please restart script!"
+      exit 1
   fi
 else
-  rm update.info > /dev/null
   echo "Connection to server failed! Are you online?"
 fi
 # if no update than clean
 echo
 echo "Do you want to clean? Y/n"; read clean
-if [[ "$clean" == "n" ]]; then
+if [ "$clean" == "n" ]
+then
     echo "canceling opertaion..."
     exit 1
 else
     echo "cleaning, might take some time!"
 fi
-apt-get autoremove -y > /dev/null
-apt-get clean -y > /dev/null
-rm -rf /var/lib/apt/lists/* > /dev/null
+apt-get autoremove -y --quiet
+apt-get clean -y --quiet
+rm -rf /var/lib/apt/lists/*
 echo "done! Updating package list..."
-apt-get update > /dev/null
+sleep 2
+apt-get update
 clear
 echo "successfully completed.
 Thanks for using tidyup by TechnikAmateur!"
-exit 0
+exit 1
